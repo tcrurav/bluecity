53c1a3285955d6f20f9cf3a06e95c450838a75f2
aaron <aaronmedmel@gmail.com>
2021-02-10 09:49:33 +0000

    MERGE feature/azure

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,52 +7,52 @@ require('dotenv').config();
 // }
 
 // PostGres
-module.exports = {
-  development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  },
-  production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL'
-  }
-}
-
-// MySQL
 // module.exports = {
 //   development: {
 //     username: process.env.MYSQL_USER,
 //     password: process.env.MYSQL_PASSWORD,
 //     database: process.env.MYSQL_DATABASE,
 //     host: process.env.DB_HOST,
-//     dialect: 'mysql'
+//     dialect: 'postgres'
 //   },
 //   test: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE,
+//     username: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DB,
 //     host: process.env.DB_HOST,
-//     dialect: 'mysql'
+//     dialect: 'postgres'
 //   },
 //   production: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE,
+//     username: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DB,
 //     host: process.env.DB_HOST,
-//     dialect: 'mysql'
+//     dialect: 'postgres',
+//     use_env_variable: 'DATABASE_URL'
 //   }
 // }
+
+// MySQL
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+}
