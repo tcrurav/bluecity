4d2d0a85bf677a9c149787f12886e8a87015cd1e
Carlos <nxssiedev@gmail.com>
2021-02-17 11:47:09 +0000

    MERGE feature/material

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 // }
 
 // PostGres
-module.exports = {
-  development: {
+/*module.exports = {
+   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
@@ -30,29 +30,29 @@ module.exports = {
     dialect: 'postgres',
     use_env_variable: 'DATABASE_URL'
   }
-}
+} */
 
-// MySQL
-// module.exports = {
-//   development: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE,
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql'
-//   },
-//   test: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE,
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql'
-//   },
-//   production: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE,
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql'
-//   }
-// }
+//MySQL
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+}
