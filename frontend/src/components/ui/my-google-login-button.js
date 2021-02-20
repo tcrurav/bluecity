3aab8275056811083c81ba8df365a07f5d79a25e
Carlos <nxssiedev@gmail.com>
2021-02-20 12:13:44 +0000

    CHANGE Google login button

--- frontend/src/components/ui/my-google-login-button.js
+++ frontend/src/components/ui/my-google-login-button.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { capitalize } from '@material-ui/core';
 
 const MyImg = styled.img`
   width: 20px;
@@ -29,7 +30,11 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#fff',
     '&:hover': {
       boxShadow: '0, 0, 16px #4285f4',
-    }
+    },
+    border: '1px solid',
+    borderRadius: '.25rem',
+    padding: '.375rem .75rem',
+    textTransform: 'capitalize',
   },
 }));
 
