213316fffc8e7e707e868709d1258226f71c7a0d
Carlos <nxssiedev@gmail.com>
2021-04-19 19:07:10 +0100

    UI Changes

---
+++ frontend/src/components/ui/footer.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { Container, Typography, Grid, Link } from "@material-ui/core";
+import Image from "material-ui-image";
+import {useTranslation} from 'react-i18next';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#00569F",
+    position: "absolute",
+    display: "flex",
+    bottom: 0,
+    minHeight: '10vh',
+    width: "100%"
+  },
+  imageGrid: {
+    maxWidth: "6em",
+    maxHeight: "auto",
+    paddingTop: "1vh",
+    paddingBottom: "1vh"
+  },
+  textGrid: {
+    textAlign: "center",
+    paddingTop: "1vh",
+    paddingBottom: "1vh",
+    color: "#fafafa",
+  },
+  links: {
+    "&:hover, visited": {
+      color: "#fafafa",
+    }
+  }
+}));
+
+export function Footer() {
+  const classes = useStyles();
+  const { t } = useTranslation();
+
+  return (
+    <div className={classes.root}>
+      <Grid container >
+        <Grid item xs={2} md={4} className={classes.imageGrid}>
+          <Image src="img/bluecity-favicon.png" aspectRatio={1/1} color=""/>
+        </Grid>
+        <Grid item xs={10} md={8} className={classes.textGrid}>
+          <Typography variant="body1" fontWeight="fontWeightBold">{t('Participating Organizations')}</Typography>
+          <Typography variant="body2">
+            <Link href="http://www.ieselrincon.es" color="inherit" className={classes.links}>IES El rincón</Link>
+          </Typography>
+          <Typography variant="body2">
+            <Link href="https://moodle.bernatelferrer.cat/" color="inherit" className={classes.links}>Institut Bernat el Ferrer</Link>
+          </Typography>
+          <Typography variant="body2">
+            <Link href="http://iesesteveterradas.cat/" color="inherit" className={classes.links}>Institut Esteve Terradas</Link>
+          </Typography>
+          <Typography variant="body2">
+            <Link href="https://www.furiouskoalas.com/" color="inherit" className={classes.links}>Furious Koalas</Link>
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
